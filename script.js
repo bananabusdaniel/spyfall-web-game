bc2caf02ae7248bd969badae245396fa45b2c493
bananabusdaniel <reDblue0>
2025-09-08 21:39:34 +0300

    [feat] Add and remove new player

--- script.js
+++ script.js
@@ -1,14 +1,31 @@
 const textInput = document.getElementById("player-name-input");
 const addPlayerBtn = document.getElementById("add-player");
 const rmvPlayerBtn = document.getElementById("remove-player");
+const playerList = document.querySelector(".player-list ul");
 
 addPlayerBtn.addEventListener("click", addPlayer);
 rmvPlayerBtn.addEventListener("click", rmvPlayer);
 
+activePlayerList = [];
+
 function addPlayer(){
-    if(textInput.value = ''){
-        throw new Error("Input name in text field.")
-    }else{
-        
+    const name = textInput.value.trim();
+    console.log(name);
+    if (name === '') {
+        alert("Please enter a player name.");
+        textInput.focus();
+        return;
     }
+
+    const newPlayer = document.createElement("li");
+    newPlayer.textContent = name;
+    playerList.appendChild(newPlayer);
+    activePlayerList.append(name);
+
+    textInput.value = '';
+}
+
+function rmvPlayer(){
+    playerList.removeChild(playerList.lastElementChild)
+    activePlayerList.pop();
 }
