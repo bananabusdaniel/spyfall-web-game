7bd5c470ed959f66fae35c943d51aa84e74206b8
bananabusdaniel <reDblue0>
2025-09-10 16:27:13 +0300

    [feat] PWA relevant files for mobile use

---
+++ sw.js
@@ -0,0 +1,14 @@
+const CACHE = "spyfall";
+const ASSETS = ["/", "/index.html", "/style.css", "/script.js"];
+
+self.addEventListener("install", e => {
+  e.waitUntil(
+    caches.open(CACHE).then(cache => cache.addAll(ASSETS))
+  );
+});
+
+self.addEventListener("fetch", e => {
+  e.respondWith(
+    caches.match(e.request).then(resp => resp || fetch(e.request))
+  );
+});
